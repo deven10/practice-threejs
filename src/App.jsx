516208cf2a254e71d5a34ab87b027a7189f4e425
Deven Umrania <deven@neelnetworks.com>
2024-02-07 15:28:00 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
     <>
       <h2>Three JS Practice</h2>
 
-      <Katana />
+      {/* <Katana /> */}
       {/* <Damascus /> */}
-      {/* <Truck /> */}
+      <Truck />
     </>
   );
 }
